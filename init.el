66b5418e8f6deb067a4ebffad388c24672f4f211
silk <silk@khel.dar>
2018-07-16 20:22:54 -0700

    made background dot a tad more subtle

--- init.el
+++ init.el
@@ -179,4 +179,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Code Retina" :foundry "outline" :slant normal :weight normal :height 158 :width normal)))))
+ '(default ((t (:family "Fira Code Retina" :foundry "outline" :slant normal :weight normal :height 158 :width normal))))
+ '(whitespace-space ((t (:background "nil" :foreground "gray25")))))
