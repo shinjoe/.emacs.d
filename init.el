6be348bec699a47c12b39682b028fb174c0a3539
Silk <silk@khel.dar>
2019-06-24 15:08:12 -0700

    fix whitespace background being too dark on mac

--- init.el
+++ init.el
@@ -210,4 +210,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Fira Code Retina" :foundry "outline" :slant normal :weight normal :height 158 :width normal))))
- '(whitespace-space ((t (:background "nil" :foreground "gray25")))))
+ '(whitespace-space ((t (:background nil :foreground "gray25")))))
