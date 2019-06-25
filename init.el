7f0025bd1ae43c28e04fc3414679788397269267
silk <silk@khel.dar>
2019-06-25 13:12:35 -0700

    change gc values while loading init.el

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
 ;; -*- lexical-binding: t; -*-
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -198,6 +201,10 @@
 (add-hook 'with-editor-mode-hook 'evil-insert-state)
 (add-hook 'python-mode-hook 'global-flycheck-mode)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(add-hook 'emacs-startup-hook
+          '(lambda()(setq gc-cons-threshold 16777216
+                     gc-cons-percentage 0.1)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
