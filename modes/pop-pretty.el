c385769405f83e007802bcb32c2ccd4f52b0033c
silk <silk@khel.dar>
2019-09-29 13:49:31 -0700

    rename ppm -> prettify

--- modes/pop-pretty.el
+++ modes/pop-pretty.el
@@ -1,13 +1,13 @@
 ;;; -*- lexical-binding: t -*-
 
-(defvar ppm-was-enabled prettify-symbols-mode)
+(defvar prettify-was-enabled prettify-symbols-mode)
 
 (define-minor-mode pop-pretty-mode
   "temporarily disables pretty-mode - python slows down when rendering prettified logical operands"
   :lighter " ppm"
   (if pop-pretty-mode
     (prettify-symbols-mode -1)
-    (when ppm-was-enabled
+    (when prettify-was-enabled
       (prettify-symbols-mode t))))
 
 ;;; pop-pretty.el ends here
